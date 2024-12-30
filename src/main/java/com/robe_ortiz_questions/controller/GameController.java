17835c812cd1c0e236e4ce6057ccd095ded2df3f
Robe Ortiz <robeortizlopez@gmail.com>
2024-12-30 10:58:32 +0100

    refactor GameController

--- src/main/java/com/robe_ortiz_questions/controller/GameController.java
+++ src/main/java/com/robe_ortiz_questions/controller/GameController.java
@@ -5,7 +5,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
+import com.robe_ortiz_questions.entity.game.Game;
 import com.robe_ortiz_questions.entity.question.CategoryOfQuestion;
 import com.robe_ortiz_questions.entity.question.MultipleQuestion;
 import com.robe_ortiz_questions.entity.question.Question;
@@ -24,130 +24,130 @@ import com.robe_ortiz_questions.service.GameService;
 
 @Controller
 @RequestMapping("/game")
-@SessionAttributes({"questions", "currentQuestionIndex", "correctAnswers", "wrongAnswers"})
+@SessionAttributes({"game"})
 public class GameController {
-	
-	private static Random random = new Random();
-	
-	@Autowired
-	private GameService gameService;
-	
 
+    @Autowired
+    private GameService gameService;
+    
+    private static Random random = new Random();
 
-	@GetMapping("/category")
-	public String selectCategory(Model model) {
-		model.addAttribute("categories", CategoryOfQuestion.values());
-		model.addAttribute("showCategoryForm", true);
-		return"game-options";
-	}
-	
-	@GetMapping("/number-of-questions")
-	public String selectNumberOfQuestions(@RequestParam("category") String category, Model model) {
-		model.addAttribute("showNumberOfQuestionsForm", true);
-		model.addAttribute("category", category);
-		return "game-options";
-	}
-	
-	@PostMapping("/answer")
-	public String answer(@RequestParam("answer") List<String> answers,  // Recibir todas las respuestas seleccionadas
-	                     @ModelAttribute("questions") List<Question> questions,
-	                     @ModelAttribute("currentQuestionIndex") int currentQuestionIndex,
-	                     @ModelAttribute("correctAnswers") int correctAnswers,
-	                     @ModelAttribute("wrongAnswers") int wrongAnswers,
-	                     Model model) {
-	    Question currentQuestion = questions.get(currentQuestionIndex);
+    @GetMapping("/category")
+    public String selectCategory(Model model) {
+        model.addAttribute("categories", CategoryOfQuestion.values());
+        model.addAttribute("showCategoryForm", true);
+        return "game-options";
+    }
 
-	    if (currentQuestion instanceof TrueOrFalseQuestion trueOrFalseQuestion) {
-	        if (Boolean.parseBoolean(answers.get(0)) == trueOrFalseQuestion.getAnswer()) {
-	            correctAnswers++;
-	        } else {
-	            wrongAnswers++;
-	        }
-	    } else if (currentQuestion instanceof MultipleQuestion multipleQuestion) {
-	        List<String> correctAnswersList = multipleQuestion.getCorrectAnswers();
-	        
-	        if (correctAnswersList.containsAll(answers)) {
-	            correctAnswers++;
-	        } else {
-	            wrongAnswers++;
-	        }
-	    }
+    @GetMapping("/number-of-questions")
+    public String selectNumberOfQuestions(@RequestParam("category") String category, Model model) {
+        model.addAttribute("showNumberOfQuestionsForm", true);
+        model.addAttribute("category", category);
+        return "game-options";
+    }
 
-	    currentQuestionIndex++;
-	    model.addAttribute("currentQuestionIndex", currentQuestionIndex);
-	    model.addAttribute("correctAnswers", correctAnswers);
-	    model.addAttribute("wrongAnswers", wrongAnswers);
+    @PostMapping("/answer")
+    public String answer(@RequestParam("answer") List<String> answers, 
+                         @ModelAttribute("game") Game game,
+                         Model model) {
+        int currentQuestionIndex = game.getCurrentQuestionIndex();
+        List<Question> questions = game.getListOfQuestions();
+        Question currentQuestion = questions.get(currentQuestionIndex);
 
-	    if (currentQuestionIndex < questions.size()) {
-	        return showQuestion(model);
-	    } else {
-	        return "redirect:/game/result";
-	    }
-	}
+        // Lógica para verificar si la respuesta es correcta
+        if (currentQuestion instanceof TrueOrFalseQuestion trueOrFalseQuestion) {
+            if (Boolean.parseBoolean(answers.get(0)) == trueOrFalseQuestion.getAnswer()) {
+                game.incrementCorrectAnswers();
+            } else {
+                game.incrementWrongAnswers();
+            }
+        } else if (currentQuestion instanceof MultipleQuestion multipleQuestion) {
+            List<String> correctAnswersList = multipleQuestion.getCorrectAnswers();
+            if (correctAnswersList.containsAll(answers)) {
+                game.incrementCorrectAnswers();
+            } else {
+                game.incrementWrongAnswers();
+            }
+        }
+
+        game.incrementCurrentQuestionIndex();
+        model.addAttribute("game", game);
+
+        if (game.getCurrentQuestionIndex() < questions.size()) {
+            return showQuestion(model);
+        } else {
+            return "redirect:/game/result";
+        }
+    }
 
     private String showQuestion(Model model) {
-        List<Question> questions = (List<Question>) model.getAttribute("questions");
-        int currentQuestionIndex = (int) model.getAttribute("currentQuestionIndex");
+        Game game = (Game) model.getAttribute("game");
+        List<Question> questions = game.getListOfQuestions();
+        int currentQuestionIndex = game.getCurrentQuestionIndex();
 
         Question question = questions.get(currentQuestionIndex);
         model.addAttribute("question", question);
 
         if (question instanceof MultipleQuestion multipleQuestion) {
-        	boolean showMultipleCorrectAnswers = false;
+            boolean showMultipleCorrectAnswers = false;
+            List<String> allAnswers = new ArrayList<>();
+            List<String> shuffledCorrectAnswers = new ArrayList<>(multipleQuestion.getCorrectAnswers());
+            List<String> shuffledIncorrectAnswers = new ArrayList<>(multipleQuestion.getIncorrectAnswers());
 
-        	List<String> allAnswers = new ArrayList<>();      	           	    
-    	    List<String> shuffledCorrectAnswers = new ArrayList<>(multipleQuestion.getCorrectAnswers());
-    	    List<String> shuffledIncorrectAnswers = new ArrayList<>(multipleQuestion.getIncorrectAnswers());
-    	    
-    	    Collections.shuffle(shuffledCorrectAnswers);
-    	    Collections.shuffle(shuffledIncorrectAnswers);
-    	    
-    	    if(shuffledCorrectAnswers.size() > 1) {
-    	    	showMultipleCorrectAnswers = random.nextBoolean();
-    	    }
-    	    	
+            Collections.shuffle(shuffledCorrectAnswers);
+            Collections.shuffle(shuffledIncorrectAnswers);
+
+            if (shuffledCorrectAnswers.size() > 1) {
+                showMultipleCorrectAnswers = random.nextBoolean();
+            }
+
             if (showMultipleCorrectAnswers) {
                 allAnswers.addAll(shuffledCorrectAnswers.subList(0, 2));
-                allAnswers.addAll(shuffledIncorrectAnswers.subList(0, 2)); 
+                allAnswers.addAll(shuffledIncorrectAnswers.subList(0, 2));
             } else {
                 allAnswers.add(shuffledCorrectAnswers.get(0));
-                allAnswers.addAll(shuffledIncorrectAnswers.subList(0, 3)); 
+                allAnswers.addAll(shuffledIncorrectAnswers.subList(0, 3));
             }
-              	    
-    	    
-    	    Collections.shuffle(allAnswers);
-    	    model.addAttribute("answers", allAnswers);
-    	    model.addAttribute("showMultipleCorrectAnswers", showMultipleCorrectAnswers);
+
+            Collections.shuffle(allAnswers);
+            model.addAttribute("answers", allAnswers);
+            model.addAttribute("showMultipleCorrectAnswers", showMultipleCorrectAnswers);
         }
 
         return "game";
     }
-          
 
     @GetMapping("/result")
     public String showResults(Model model) {
-    	model.addAttribute("showResult",true);
-        model.addAttribute("correctAnswers", model.getAttribute("correctAnswers"));
-        model.addAttribute("wrongAnswers", model.getAttribute("wrongAnswers"));
+        Game game = (Game) model.getAttribute("game");
+        model.addAttribute("showResult", true);
+        model.addAttribute("correctAnswers", game.getCorrectAnswers());
+        model.addAttribute("wrongAnswers", game.getWrongAnswers());
         return "game-result";
     }
-	
-	@GetMapping("/play")
-	public String play(@RequestParam("category") String category, @RequestParam("numberOfQuestions") String numberOfQuestions, Model model) { 	
-		
-		try {		
-			List<Question> questions = gameService.getQuestions(Integer.parseInt(numberOfQuestions), CategoryOfQuestion.valueOf(category));
-			
-			if(questions.size() < Integer.parseInt(numberOfQuestions)) throw new IllegalArgumentException();
-            model.addAttribute("questions", questions);
-            model.addAttribute("currentQuestionIndex", 0);
-            model.addAttribute("correctAnswers", 0);
-            model.addAttribute("wrongAnswers", 0);			
+
+    @GetMapping("/play")
+    public String play(@RequestParam("category") String category, 
+                       @RequestParam("numberOfQuestions") String numberOfQuestions, 
+                       Model model) {
+        try {
+            List<Question> questions = gameService.getQuestions(Integer.parseInt(numberOfQuestions), CategoryOfQuestion.valueOf(category));
+
+            if (questions.size() < Integer.parseInt(numberOfQuestions)) {
+                throw new IllegalArgumentException();
+            }
+
+            Game game = new Game(Integer.parseInt(numberOfQuestions), 
+                                 CategoryOfQuestion.valueOf(category), 
+                                 questions);
+
+            model.addAttribute("game", game);
+
             return showQuestion(model);
-            
-		}catch (IllegalArgumentException e) {			
+
+        } catch (IllegalArgumentException e) {
             model.addAttribute("error", "Actualmente no podemos servir la configuración solicitada.");
-            return "game";           
-		}
-	}
+            return "game";
+        }
+    }
 }
--- src/main/java/com/robe_ortiz_questions/entity/game/Game.java
+++ src/main/java/com/robe_ortiz_questions/entity/game/Game.java
@@ -6,27 +6,53 @@ import com.robe_ortiz_questions.entity.question.CategoryOfQuestion;
 import com.robe_ortiz_questions.entity.question.Question;
 
 public class Game {
+    private int numberOfQuestions;
+    private CategoryOfQuestion categoryOfQuestions;
+    private List<Question> listOfQuestions;
+    private int currentQuestionIndex = 0;
+    private int correctAnswers = 0;
+    private int wrongAnswers = 0;
 
-	private int numberOfQuestions;
-	private CategoryOfQuestion categoryOfQuestions;
-	private List<Question> listOfQuestions;
+    public Game(int numberOfQuestions, CategoryOfQuestion categoryOfQuestions, List<Question> listOfQuestions) {
+        this.numberOfQuestions = numberOfQuestions;
+        this.categoryOfQuestions = categoryOfQuestions;
+        this.listOfQuestions = listOfQuestions;
+    }
 
-	public Game(int numberOfQuestions, CategoryOfQuestion categoryOfQuestions, List<Question> listOfQuestions) {
-		this.numberOfQuestions = numberOfQuestions;
-		this.categoryOfQuestions = categoryOfQuestions;
-		this.listOfQuestions = listOfQuestions;
-	}
+    public void incrementCurrentQuestionIndex() {
+        this.currentQuestionIndex++;
+    }
 
-	public int getNumberOfQuestions() {
-		return numberOfQuestions;
-	}
+    public void incrementCorrectAnswers() {
+        this.correctAnswers++;
+    }
 
-	public CategoryOfQuestion getCategoryOfQuestions() {
-		return categoryOfQuestions;
-	}
+    public void incrementWrongAnswers() {
+        this.wrongAnswers++;
+    }
 
-	public List<Question> getListOfQuestions() {
-		return listOfQuestions;
-	}
-		
+    public int getCurrentQuestionIndex() {
+        return currentQuestionIndex;
+    }
+
+    public int getCorrectAnswers() {
+        return correctAnswers;
+    }
+
+    public int getWrongAnswers() {
+        return wrongAnswers;
+    }
+
+    public int getNumberOfQuestions() {
+        return numberOfQuestions;
+    }
+
+    public CategoryOfQuestion getCategoryOfQuestions() {
+        return categoryOfQuestions;
+    }
+
+    public List<Question> getListOfQuestions() {
+        return listOfQuestions;
+    }
 }
+
